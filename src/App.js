5f09ce86641f6411a2c0a4d6c6c7ff1682335d6f
ksairatna37 <ksairatna37@gmail.com>
2025-01-14 13:09:15 +0530

    added hashroute

--- src/App.js
+++ src/App.js
@@ -1,39 +1,115 @@
+// import './App.css';
+// import React,{useEffect} from 'react';
+// import Hero from './Components/hero';
+// import Items from './Components/items';
+// import Cart from './Components/cart';
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import Choose from './Components/choose';
+// import Orders from './Components/orders';
+// import './Styles/responsive.css'
+// import { saveToTxtFile } from "./Components/save";
+
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/",
+//       element: <Hero />
+//     },
+//     {
+//       path: "/items",
+//       element: <Items />
+//     },
+//     {
+//       path: "/items/:pizzaname/:price",
+//       element: <Cart />
+//     },
+//     {
+//       path: "/items/:pizzaname/choose",
+//       element: <Choose />
+//     },
+//     {
+//       path: "/myorders",
+//       element: <Orders />
+//     },
+
+//   ]
+// )
+// function App() {
+//   useEffect(() => {
+//     function checkAndSaveOrders() {
+//       const currentDate = new Date().toLocaleDateString();
+//       const lastSavedDate = localStorage.getItem("lastSavedDate");
+
+//       if (lastSavedDate !== currentDate) {
+//         const orders = JSON.parse(localStorage.getItem("orders")) || [];
+
+//         const orderDetails = orders.map(order =>
+//           `Order ID: ${order.orderId}\nPizza: ${order.pizzaname}\nDate: ${order.orderDate}\n`
+//         ).join("\n");
+
+//         saveToTxtFile(orderDetails, `${currentDate}.txt`);
+
+//         localStorage.removeItem("orders");
+//         localStorage.setItem("lastSavedDate", currentDate);
+//       }
+//     }
+
+
+//     const interval = setInterval(() => {
+//       checkAndSaveOrders();
+//     }, 86400000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+//   return (
+//     <div className="App">
+//       <RouterProvider router={router}></RouterProvider>
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Hero from './Components/hero';
 import Items from './Components/items';
 import Cart from './Components/cart';
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+
 import Choose from './Components/choose';
 import Orders from './Components/orders';
 import './Styles/responsive.css'
 import { saveToTxtFile } from "./Components/save";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Hero />
-    },
-    {
-      path: "/items",
-      element: <Items />
-    },
-    {
-      path: "/items/:pizzaname/:price",
-      element: <Cart />
-    },
-    {
-      path: "/items/:pizzaname/choose",
-      element: <Choose />
-    },
-    {
-      path: "/myorders",
-      element: <Orders />
-    },
+// const router = createBrowserRouter(
+//   [
+//     {
+//       path: "/",
+//       element: <Hero />
+//     },
+//     {
+//       path: "/items",
+//       element: <Items />
+//     },
+//     {
+//       path: "/items/:pizzaname/:price",
+//       element: <Cart />
+//     },
+//     {
+//       path: "/items/:pizzaname/choose",
+//       element: <Choose />
+//     },
+//     {
+//       path: "/myorders",
+//       element: <Orders />
+//     },
 
-  ]
-)
+//   ]
+// )
 function App() {
   useEffect(() => {
     function checkAndSaveOrders() {
@@ -62,9 +138,15 @@ function App() {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="App">
-      <RouterProvider router={router}></RouterProvider>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/items/:pizzaname/:price" element={<Cart />} />
+        <Route path="/items/:pizzaname/choose" element={<Choose />} />
+        <Route path="/myorders" element={<Orders />} />
+      </Routes>
+    </HashRouter>
 
   );
 }
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Styles/navbar.css'
+import { Link } from "react-router-dom";
 export default function navbar() {
     return (
         <>
@@ -7,9 +8,14 @@ export default function navbar() {
                 <a class="navbar-brand mx-5 my-3" href="/">TRUE PIZZA</a>
                 <ul>
                     
-                    <li><a href="/items">Pizza's</a></li>
-                    <li><a href="/myorders">My Orders</a></li>
-                    
+                    {/* <li><a href="/items">Pizza's</a></li>
+                    <li><a href="/myorders">My Orders</a></li> */}
+                    <li>
+                        <Link to="/items">Pizza's</Link>
+                    </li>
+                    <li>
+                        <Link to="/myorders">My Orders</Link>
+                    </li>
                 </ul>
             </nav>
 
